48ae8d77417920e68a89eef96efdb4b689f088b5
andrn <01.nmeml@ya.ru>
2025-07-27 16:35:33 +0300

    corrected Character.js

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,11 +1,10 @@
 export default class Character {
   constructor(name, type, health=100, level=1, attack=10, defence=10) {
-    if (typeof name === 'string' && typeof type === 'string' && name.length > 1 && name.length < 11 && ['Character', 'Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'].includes(type)) {
-      this.name = name
-      this.type = type
-    } else {
+    if (typeof name !== 'string' || typeof type !== 'string' || name.length < 2 || name.length > 10 || !['Character', 'Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'].includes(type)) {
       throw new Error("Incorrect data!")
     }
+    this.name = name
+    this.type = type
     this.health = health
     this.level = level
     this.attack = attack
